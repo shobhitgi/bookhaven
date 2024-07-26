ee7eed2affd09c182c19cd175424be6118029eda
Shobhit Sinha <101276041+shobhitgi@users.noreply.github.com>
2024-07-26 14:23:42 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,14 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app",
+        method: ["POST", "GET"],
+        credentials: true
+    }
+));
+
 app.use(express.json());
 
 dotenv.config();
